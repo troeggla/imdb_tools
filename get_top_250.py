995d9d4225a5e51ddd3cb2f8ffea102596434761
Thomas Röggla <troeggla@users.noreply.github.com>
2016-11-25 13:56:38 +0100

    added script which fetches IMDB's Top 250 list from imdb.com and parses it

---
+++ get_top_250.py
@@ -0,0 +1,37 @@
+import codecs
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+sys.stdout = codecs.getwriter("utf8")(sys.stdout)
+
+
+def get_top_250():
+    data = requests.get("http://www.imdb.com/chart/top", headers={
+        "Accept-Language": "en"
+    })
+    doc = BeautifulSoup(data.text, "html.parser")
+    table = doc.find(class_="lister").find("tbody", class_="lister-list")
+
+    for row in table.find_all("tr"):
+        title_col = row.find(class_="titleColumn")
+        rating_col = row.find(class_="imdbRating")
+
+        position = int(title_col.contents[0].strip()[:-1])
+        title = title_col.a.contents[0]
+        year = int(title_col.span.contents[0].strip()[1:-1])
+        rating = float(rating_col.strong.contents[0])
+
+        yield position, title, year, rating
+
+
+def main():
+    top250 = get_top_250()
+
+    print "position,title,year,rating"
+    for position, title, year, rating in top250:
+        print u'%d,"%s",%d,%.1f' % (position, title, year, rating)
+
+
+if __name__ == "__main__":
+    main()
