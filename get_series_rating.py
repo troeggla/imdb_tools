92da9b3c118ee52626fd1debe1877541f0477912
Thomas Röggla <troeggla@users.noreply.github.com>
2018-06-04 15:57:18 +0200

    attempting to adapt to IMDB redesign

--- get_series_rating.py
+++ get_series_rating.py
@@ -1,22 +1,58 @@
 import sys
+import re
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+def parse_episodes(doc):
+    eplist = doc.find("div", class_="eplist").find_all(
+        itemtype="http://schema.org/TVEpisode"
+    )
+
+    for episode in eplist:
+        try:
+            airdate = datetime.strptime(
+                episode.find(class_="airdate").text.strip(),
+                "%d %b. %Y"
+            )
+
+            title = episode.strong.text
+            epnum = episode.find("meta", itemprop="episodeNumber")["content"]
+
+            ratings = episode.find(class_="ipl-rating-star").find_all("span")
+            rating = float(ratings[1].text)
+            num_votes = int(re.sub(r"[^0-9]", "", ratings[2].text))
+
+            yield epnum, title, rating, num_votes
+        except ValueError:
+            pass
 
 
 def get_series_ratings(imdb_id):
-    url = "http://www.imdb.com/title/" + imdb_id + "/epdate"
+    url = "http://www.imdb.com/title/" + imdb_id + "/episodes"
     data = requests.get(url, headers={
         "Accept-Language": "en"
     })
 
     doc = BeautifulSoup(data.text, "html.parser")
-    table = doc.find(id="tn15content").find("table").find_all("tr")
+    seasons = doc.find("select", id="bySeason").find_all("option")
 
-    for row in table[1:]:
-        cells = row.find_all("td")[:4]
-        num, name, rating, count = [e.text.strip() for e in cells]
+    preselected_episodes = parse_episodes(doc)
 
-        yield num, name, float(rating), int(count.replace(",", ""))
+    for season in seasons:
+        if season.has_attr("selected"):
+            for episode in preselected_episodes:
+                yield (season["value"] + "." + episode[0],) + episode[1:]
+        else:
+            season_url = url + "?season=" + season["value"]
+            data = requests.get(season_url, headers={
+                "Accept-Language": "en"
+            })
+
+            doc = BeautifulSoup(data.text, "html.parser")
+            for episode in parse_episodes(doc):
+                yield (season["value"] + "." + episode[0],) + episode[1:]
 
 
 def main():
